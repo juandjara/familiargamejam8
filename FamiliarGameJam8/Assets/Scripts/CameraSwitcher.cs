b9f48e45acb48c0f9013b6cc3c70c7b218c5422d
Juan D. Jara <juanorigami@gmail.com>
2017-03-25 11:02:00 +0100

    camera toggle scripts and test scenes

--- FamiliarGameJam8/Assets/Scripts/CameraSwitcher.cs
+++ FamiliarGameJam8/Assets/Scripts/CameraSwitcher.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class CameraSwitcher : MonoBehaviour {
 
-	public Camera leftCamera;
-    public Camera rightCamera;
+	public Camera[] cameras;
+	public static CameraSwitcher instance;
 
-    public void ShowOverheadView() {
-        leftCamera.enabled = false;
-        rightCamera.enabled = true;
-    }
-    
-    public void ShowFirstPersonView() {
-        leftCamera.enabled = true;
-        rightCamera.enabled = false;
-    }
+	void Awake() {
+		if(instance == null) {
+			instance = this;
+		} else if(instance != this) {
+			Destroy(this.gameObject);
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+    public void enableCamera(int targetIndex) {
+		if(targetIndex < 0 || targetIndex >= cameras.Length) {
+			Debug.LogError("Invalid index. This camera does not exist");
+			return;
+		}
+		for(int index = 0; index < cameras.Length; index++) {
+			Camera cam = cameras[index];
+			if(index == targetIndex) {
+				cam.enabled = true;
+			} else {
+				cam.enabled = false;
+			}
+		}
+	}
 }
---
+++ FamiliarGameJam8/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 50f;
+	public float rotateSpeed = 100f;
+	private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float turnValue = Input.GetAxis("Horizontal");
+		float zAxis = Input.GetAxis("Vertical");
+		movement(zAxis);
+		turn(turnValue);
+	}
+
+	void movement(float zAxis) {
+		Vector3 movement = new Vector3(0, 0, zAxis) * moveSpeed * Time.deltaTime;
+		playerTransform.Translate(movement);				
+	}
+
+	void turn(float turnValue) {
+		Vector3 rotationVector = new Vector3(0, turnValue, 0) * rotateSpeed * Time.deltaTime;
+		playerTransform.Rotate(rotationVector);
+	}
+}
