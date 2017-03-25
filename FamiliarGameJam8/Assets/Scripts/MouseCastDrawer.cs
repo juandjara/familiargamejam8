9fcd54fa01800659acd673ba2b5dbf2d7c95cacc
Juan D. Jara <juanorigami@gmail.com>
2017-03-25 19:37:44 +0100

    todos los tigres

---
+++ FamiliarGameJam8/Assets/Scripts/MouseCastDrawer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCastDrawer : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)) {
+			Camera cam = Camera.main;
+			var mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+			Debug.DrawLine(transform.position, mousePos, Color.black);
+		}
+	}
+}
--- FamiliarGameJam8/Assets/Scripts/PlayerController.cs
+++ FamiliarGameJam8/Assets/Scripts/PlayerController.cs
@@ -17,13 +17,27 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		float turnValue = Input.GetAxis("Horizontal");
 		float xAxis = Input.GetAxis("Vertical");
-		movement(xAxis);
+
 		turn(turnValue);
+		
+		Ray ray = new Ray(transform.position+transform.right*0.8f, transform.right*xAxis);
+		Debug.DrawRay (ray.origin, ray.direction , Color.black);
+
+		RaycastHit hit;
+
+		//dispara rayo y lo guarda en la variable de out	
+		if (Physics.Raycast (ray.origin, ray.direction * xAxis, out hit)) {
+			if (hit.distance < 0.1f) {
+				Debug.Log (" obj " + hit.collider.gameObject.name);
+			}
+		}
+
+		movement(xAxis);
 	}
 
 	void movement(float xAxis) {
 		Vector3 movement = new Vector3(xAxis, 0, 0) * moveSpeed * Time.deltaTime;
-		playerTransform.Translate(movement);				
+		playerTransform.Translate(movement);		
 	}
 
 	void turn(float turnValue) {
