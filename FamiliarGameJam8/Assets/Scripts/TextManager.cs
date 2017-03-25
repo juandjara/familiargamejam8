b1ae7a5968d338e7de38cf3ebca438e3c77fd62f
Juan D. Jara <juanorigami@gmail.com>
2017-03-26 00:36:13 +0100

    text manager

--- FamiliarGameJam8/Assets/Scripts/TextManager.cs
+++ FamiliarGameJam8/Assets/Scripts/TextManager.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class textEvent : MonoBehaviour {
+public class TextManager : MonoBehaviour {
 
 	bool mostrandoMensaje = false;
 	float velocidadMensaje;
 	float velocidadMensajeRapido = 0.03f;
 	float velocidadMensajeLento = 0.06f;
 
+	public Text targetText;
+	public static TextManager instance;
+
 	//public delegate string _SetText();
 	//public static event _SetText SetText;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Awake() {
+		if(instance == null) {
+			instance = this;
+		} else if(instance != this) {
+			Destroy(this.gameObject);
+		}
+		DontDestroyOnLoad(this.gameObject);
 	}
 	
 	// Update is called once per frame
@@ -27,14 +34,18 @@ public class textEvent : MonoBehaviour {
 		}
 	}
 
+	public void ChangeText(string text) {
+		targetText.text = text;
+	}
+
 	public void muestraMensaje(string mensaje) {
 		StartCoroutine (muestraMensajeInterno (mensaje));
 	}
 
 	IEnumerator muestraMensajeInterno(string mensaje) {
-		GetComponent<Text> ().text += "\n";
+		targetText.text = "";
 		for (int i = 0; i < mensaje.Length; i++) {
-			GetComponent<Text> ().text += mensaje[i];
+			targetText.text += mensaje[i];
 			//TODO ejecutar sonido de tecla
 			yield return new WaitForSeconds (velocidadMensaje);
 		}
